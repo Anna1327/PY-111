d748f76b0787aa0b31f265dcd67a668a0caa0207
Anna1327 <aoi.yatsu@ya.ru>
2019-12-28 13:15:21 +0300

    linear_search and binary_search were added

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -2,12 +2,26 @@ from typing import Any, Sequence, Optional
 
 
 def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
-	"""
-	Performs binary search of given element inside of array
+    """
+    Performs binary search of given element inside of array
 
-	:param elem: element to be found
-	:param arr: array where element is to be found
-	:return: Index of element if it's presented in the arr, None otherwise
-	"""
-	print(elem, arr)
-	return None
+    :param elem: element to be found
+    :param arr: array where element is to be found
+    :return: Index of element if it's presented in the arr, None otherwise
+    """
+    if len(arr) == 0:
+        return
+    start = 0
+    stop = (len(arr))
+    while True:
+        if start == stop:
+            return None
+        middle = ((stop - start) // 2) + start
+        if elem == arr[middle]:
+            return middle
+
+        elif elem > arr[middle]:
+            start = middle + 1
+
+        elif elem < arr[middle]:
+            stop = middle
