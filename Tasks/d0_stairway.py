5c91d293ec96c152490e2466db7660272a44fb77
Anna1327 <aoi.yatsu@ya.ru>
2020-01-20 08:05:55 +0300

    added stairway + improve KMP (1 test passed, 3 - fail)

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,16 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
 	:param stairway: list of ints, where each int is a cost of appropriate step
 	:return: minimal cost of getting to the top
 	"""
-	print(stairway)
-	return 0
+	step_2 = 0
+	step_1 = stairway[0]
+	for step in range(1, len(stairway)):
+		if step_2 + stairway[step] < step_1 + stairway[step]:
+			step_2, step_1 = \
+			step_1, step_2 + stairway[step]
+		else:
+			step_2, step_1 = \
+			step_1, step_1 + stairway[step]
+
+	return step_1
+
+print(stairway_path([3, 4, 7, 8, 5, 2]))
--- Tasks/h0_KMP.py
+++ Tasks/h0_KMP.py
@@ -2,7 +2,7 @@ from typing import Optional, Sequence
 
 inp_string = "мама мыла раму"
 substr = "мыла"
-prefix_str = "мыла"
+
 i = 0
 j = 0
 
@@ -15,39 +15,39 @@ def kmp_algo(inp_string: str, substr: str) -> (int):
 	:return: index where first occurrence of substr in inp_string started or None if not found
 	"""
 
-	def prefix_fun(prefix_str: str) -> Sequence[int]:
+	def prefix_fun(substr: str) -> Sequence[int]:
 		"""
 		Prefix function for KMP
 
 		:param prefix_str: dubstring for prefix function
 		:return: prefix values table
 		"""
-		prefix_list = [0] * len(prefix_str)
-		for i in range(2, len(prefix_str) + 1):
-			sub_str = prefix_str[:i]
+		prefix_list = [0] * len(substr)
+		for i in range(2, len(substr) + 1):
+			sub_str = substr[:i]
 			for j in range(1, len(sub_str)):
 				prefix = sub_str[:j]
 				suffix = sub_str[-j:]
 				if prefix == suffix:
 					prefix_list[i - 1] = j
 
-		print(prefix_str)
 		return prefix_list
 	global i,j
-	p = prefix_fun(prefix_str)
+	p = prefix_fun(substr)
 
 	while i < len(inp_string):
-		i += 1
-		j += 1
+		if inp_string[i] == substr[j]:
+			i += 1
+			j += 1
 		if j == len(substr):
 			return i - len(substr)
-		else:
-			if i < len(inp_string) and substr[j] != inp_string[i]:
+		elif inp_string[i] != substr[j]:
 				if j == 0:
 					i += 1
 				elif j > 0:
 					j = (p[j - 1])
-	print(inp_string, substr, prefix_fun)
+
+	# print(inp_string, substr, prefix_fun)
 	return -1
 
 print(kmp_algo(inp_string, substr))
